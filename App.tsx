274f47b373b04565652fed470f313cd65600d545
futuremillionaire14 <120112238+futuremillionaire14@users.noreply.github.com>
2023-02-06 16:46:30 -0500

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Calculator } from 'react-mac-calculator'
+
+const App = props => (
+  <div className="app">
+    <Calculator />
+  </div>
+)
+
+export default App
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-calculatorrrr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-calculatorrrr)
